1ab98b443e6f111161ecefbae3257a105ff5cc5e
Sergio Nicolas Pinilla <sergio.pinilla.mt@correo.usa.edu.co>
2021-11-14 08:38:55 -0500

    Creacion del proyecto

---
+++ src/main/java/com/usa/misiontic/Herramientas/HerramientasApplication.java
@@ -0,0 +1,13 @@
+package com.usa.misiontic.Herramientas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerramientasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerramientasApplication.class, args);
+	}
+
+}
